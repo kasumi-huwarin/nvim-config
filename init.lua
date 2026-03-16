a58b14db8ca10c0e88dce21e52d0ef6ecbbc8259
ochiaikouta <otiaikouta1118@gmail.com>
2026-03-16 20:02:10 +0900

    fix: 機能ごとに分離

--- init.lua
+++ init.lua
@@ -20,34 +20,9 @@ vim.opt.rtp:prepend(lazypath)
 require("options")   -- options.lua
 require("keymaps")   -- keymaps.lua
 
+-- UIとクリップボード設定
+require("config.ui")
+require("config.wsl_clipboard")
+
 -- プラグイン設定を読み込む
 require("lazy").setup("plugins")
-
--- lspを読み込み
-require("lsp")
-
--- カラースキーム
-vim.cmd.colorscheme("vim")
-
--- 補完のテーマ
-vim.api.nvim_set_hl(0, "Pmenu", { bg = "#2e2e2e", fg = "#ffffff" })      -- 補完メニュー全体
-vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4b4b4b", fg = "#ffffff" })   -- 選択中
-vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#6c6c6c" })                -- optional: スクロールバー
-
--- wslを使う時で分岐
-if vim.fn.has("wsl") == 1 then
-  vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-end 
-vim.opt.clipboard = "unnamedplus"
-
