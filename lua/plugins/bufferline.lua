3ba85e6772c28c11ce28c52f5905489d803ded50
ochiaikouta <otiaikouta1118@gmail.com>
2026-03-16 20:56:09 +0900

    fix: bufferlineの調整

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,16 +4,24 @@ return {
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("bufferline").setup {
+    require("bufferline").setup({
       options = {
-        mode = "buffers", -- バッファ一覧を表示
-        numbers = "none", -- バッファ番号の表示方法: "none", "ordinal", "buffer_id"
-        diagnostics = "nvim_lsp", -- LSP診断を表示 (エラー数とか)
-        separator_style = "slant", -- "slant", "padded_slant", "thick", "thin"
-        show_close_icon = false, -- 全体を閉じる✕を消す
-        show_buffer_close_icons = true, -- 各バッファの✕は残す
+        mode = "buffers",
+        numbers = "none",
+        diagnostics = "nvim_lsp",
+        separator_style = "slant",
+        show_close_icon = false,
+        show_buffer_close_icons = true,
+        always_show_bufferline = true,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "Explorer",
+            text_align = "center",
+            separator = true,
+          },
+        },
       },
-    }
+    })
   end,
 }
-
